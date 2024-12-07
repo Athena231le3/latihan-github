5d0079983b8495db8b0d17e00d8f91c64f201035
Breezea <liliebreezea@gmail.com>
2024-12-07 16:23:05 +0700

    add: new line

--- git.cpp
+++ git.cpp
@@ -1,7 +1,18 @@
 #include <iostream>
 using namespace std;
 
+void world();
+
 int main () {
     cout << "Hello world!" << endl;
-    cout << ":(";
+    cout << "---------------------------" << endl;
+    cout << "|                         |" << endl;
+    cout << "|" << world() << "                        |" << endl;
+    cout << "|                         |" << endl;
+    cout << "---------------------------" << endl;
+}
+
+void world()
+{
+    cout << "Hello";
 }
